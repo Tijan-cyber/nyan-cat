28e7aae0176142492617074e58b1c563fa81a715
filip-filip-filip <djordjevic_f@icloud.com>
2026-03-19 19:26:02 +0100

    Update nyan_cat.py

--- nyan_cat.py
+++ nyan_cat.py
@@ -5,7 +5,7 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1800,1126))
 pygame.mouse.set_visible(0)
 pygame.display.set_caption("Nyan cat")
-background = pygame.image.load("./background.jpeg")  #!!!!!!!!!!!! Spremeni na svojo pot do slike
+background = pygame.image.load("background.jpeg")  #!!!!!!!!!!!! pomoje ni treba ker ji itak v istem folderju
 igra = True
 
 
