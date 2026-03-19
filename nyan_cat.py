adc41f4edc25dbeb854ccafee772b1af39c40611
filip <djordjevic_f@icloud.com>
2026-03-19 19:27:53 +0100

    o

--- nyan_cat.py
+++ nyan_cat.py
@@ -3,10 +3,15 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Nyan cat")
-background = pygame.image.load("./background")  #!!!!!!!!!!!! Spremeni na svojo pot do slike
+background = pygame.image.load("background.jpeg")  #!!!!!!!!!!!! Spremeni na svojo pot do slike
 igra = True
 
 while igra:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			igra = False
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        igra = False
+
+        pygame.display.update()
+        
+
+
