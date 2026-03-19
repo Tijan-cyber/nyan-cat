91af212eb7b8d9a25446d2d3ef35b2d801d61c7a
Tijan Košir <tijan.kosir@gmail.com>
2026-03-19 19:05:09 +0100

    background

--- nyan_cat.py
+++ nyan_cat.py
@@ -1,12 +1,22 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1000,1000))
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((1800,1126))
+pygame.mouse.set_visible(0)
 pygame.display.set_caption("Nyan cat")
-background = pygame.image.load("./background")  #!!!!!!!!!!!! Spremeni na svojo pot do slike
+background = pygame.image.load("./background.jpg")  #!!!!!!!!!!!! Spremeni na svojo pot do slike
 igra = True
 
+
+
+
 while igra:
+	clock.tick(60)
+	screen.blit(background, (0,0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			igra = False
+
+
+	pygame.display.update()
