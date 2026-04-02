484cc30b1baeb1c7efd24b4477c2cbcd022b1027
filip <djordjevic_f@icloud.com>
2026-04-02 18:36:26 +0200

    jump narejen

--- nyan_cat.py
+++ nyan_cat.py
@@ -3,8 +3,8 @@ import random
 
 pygame.init()
 clock = pygame.time.Clock()
-X = 1800
-Y = 1126
+X = 1000
+Y = 500
 screen = pygame.display.set_mode((X,Y))
 pygame.mouse.set_visible(0)
 pygame.display.set_caption("Nyan cat")
@@ -50,63 +50,94 @@ scroll = 0
 
 speed = 0.2
 
+# to pove koliko jumpov ima na rzpolago
+obrat_zanke = 0
+JUMPcount = 1
+
 
 while igra:
-	clock.tick(60)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			igra = False
-		if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:  #Začetek igre
-			start_screen = False
+        print(f"jump count{JUMPcount}")
+        obrat_zanke += 1
+        print(obrat_zanke)
+        if obrat_zanke == 2:
+                print("yay(reset na 2 jumpa)")
+                obrat_zanke=0
+                JUMPcount = 2
+        
 
-	screen.blit(font.render(f"Za začetek pritisni SPACE", True, (88, 151, 252)),(X//2-500,Y//2-100))
+
+
+
+
+
+        
+        clock.tick(60)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        igra = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:  #Začetek igre
+                        start_screen = False
+
+        screen.blit(font.render(f"Za začetek pritisni SPACE", True, (88, 151, 252)),(X//2-500,Y//2-100))
 
  #Samo za testiranje da ni  start screena
 
 
-	while not start_screen: #igranje
-		time = clock.tick(60)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				quit()
+        while not start_screen: #igranje
+                time = clock.tick(60)
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                quit()
+                
+
+
+                        if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_UP or event.key == pygame.K_SPACE or event.key == pygame.K_w:
+                                        if JUMPcount > 0:
+                                                speed = -0.5
+                                                JUMPcount -= 1
 
 
 
+                #print(f"jump count{JUMPcount}")
+                obrat_zanke += 1
+                #print(obrat_zanke)
+                if obrat_zanke == 180:
+                        print("yay(reset na 2 jumpa)")
+                        obrat_zanke=0
+                        JUMPcount = 2
 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP or event.key == pygame.K_SPACE or event.key == pygame.K_w:
-					speed = -0.5
 
-		speed += 0.01
-		cat.y += speed * time
-		ufo.x -= 1
-		ice_cream.x -= 1
-		lollipop.x -= 1
-		krof.x -= 1
-		milk.x -= 1
+                speed += 0.01
+                cat.y += speed * time
+                ufo.x -= 1
+                ice_cream.x -= 1
+                lollipop.x -= 1
+                krof.x -= 1
+                milk.x -= 1
 
-		for i in range(3):
-			screen.blit(background, (i * bg_width + scroll,0))
+                for i in range(3):
+                        screen.blit(background, (i * bg_width + scroll,0))
 
 		#scroll background
-		scroll -= 5
+                scroll -= 5
 
 		#reset scroll
-		if abs(scroll) > bg_width:
-			scroll = 0
+                if abs(scroll) > bg_width:
+                        scroll = 0
 
-		screen.blit(ufo_image, ufo)
-		screen.blit(ice_cream_image, ice_cream)
-		screen.blit(lollipop_image, lollipop)
-		screen.blit(krof_image, krof)
-		screen.blit(milk_image, milk)
-		screen.blit(cat_image, cat)
+                screen.blit(ufo_image, ufo)
+                screen.blit(ice_cream_image, ice_cream)
+                screen.blit(lollipop_image, lollipop)
+                screen.blit(krof_image, krof)
+                screen.blit(milk_image, milk)
+                screen.blit(cat_image, cat)
 
 
-		pygame.display.flip()
+                pygame.display.flip()
 
-	pygame.display.flip()
-	screen.blit(background, (0,0))
+        pygame.display.flip()
+        screen.blit(background, (0,0))
 
 
 
